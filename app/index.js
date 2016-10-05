82801a3d229a44b8f7a350302428b5db9d4adf52
lihongji <m18513622209@163.com>
2016-10-05 17:07:57 +0800

    add

--- app/index.js
+++ app/index.js
@@ -4,4 +4,29 @@ import ReactDOM from 'react-dom';
 import App from './container/App';
 
 let root = document.getElementById('app');
-ReactDOM.render( <App />, root );
+
+
+let  renderByType = (type, value ,innerHTML) => {
+  const Component = type
+  return <Component value={value}>{innerHTML}</Component>
+}
+
+let  renderByCopy = (type, props ,children) => {
+  const Component = type
+  return React.cloneElement(
+    <Component />,
+    props,
+    children,
+  )
+}
+
+let Container = () => {
+  return <div>
+    <div>{renderByType('input','renderByType')}</div>
+    <div>{renderByType('div',null,'renderByType')}</div>
+    <div>{renderByCopy('input',{value:'renderByCopy'})}</div>
+    <App />
+  </div>
+}
+
+ReactDOM.render( <Container />, root );
